ff49ae02f94a2e70c33f3fd60ce4e904d4ff3eb7
huaweiwei <harvey0379@163.com>
2021-11-29 09:49:03 +0800

    groovy demo

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## run
 
 `mvn spring-boot:run`
+
+## cli
+
+run `app.groovy`: `spring run app.groovy`
---
+++ app.groovy
@@ -0,0 +1,7 @@
+@RestController
+class ThisWillActuallyRun {
+    @RequestMapping("/")
+    String home() {
+        "Hello World!1111111"
+    }
+}
