abea88aaff88082b06ed5d2bc4b0eddaf48833d3
huaweiwei <harvey0379@163.com>
2021-11-29 23:02:36 +0800

    controller

--- src/main/java/com/huauauaa/springboot/SpringbootApplication.java
+++ src/main/java/com/huauauaa/springboot/SpringbootApplication.java
@@ -2,20 +2,15 @@ package com.huauauaa.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+
 public class SpringbootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
     }
 
-    @RequestMapping("/")
-    String home() {
-        return "Hello World";
-    }
+
 
 }
