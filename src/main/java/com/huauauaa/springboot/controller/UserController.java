a96b7e6b5977524beca4892bcc083c6bae9e3c4b
huaweiwei <harvey0379@163.com>
2022-01-14 00:53:02 +0800

    feat: Page and Query

--- src/main/java/com/huauauaa/springboot/controller/UserController.java
+++ src/main/java/com/huauauaa/springboot/controller/UserController.java
@@ -1,13 +1,15 @@
 package com.huauauaa.springboot.controller;
 
-import java.util.List;
-
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.OrderItem;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.huauauaa.springboot.entity.User;
 import com.huauauaa.springboot.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,7 +20,15 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/list")
-    public List<User> list() {
-        return userService.list();
+    public Page<User> list(@RequestParam(defaultValue = "1", required = false) Integer currentPage,
+            @RequestParam(defaultValue = "10", required = false) Integer pageSize,
+            @RequestParam(defaultValue = "", required = false) String q) {
+        QueryWrapper queryWrapper = new QueryWrapper<User>();
+        if (q != null) {
+            queryWrapper.like("firstName", q);
+        }
+        Page<User> pager = new Page<>(currentPage, pageSize);
+        pager.addOrder(OrderItem.desc("id"));
+        return userService.page(pager, queryWrapper);
     }
 }
